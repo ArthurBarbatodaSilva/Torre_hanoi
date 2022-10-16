4b0b1746485012e64b4c7f5b202723f30a10db3e
DanielSouzaCruz <danielrenovar165@gmail.com>
2022-10-16 18:38:01 -0300

    Colocar disco,ultimo disco,main

--- Hanoi/Hanoi.py
+++ Hanoi/Hanoi.py
@@ -3,11 +3,23 @@ class Torre:
         self._torre = torre
         self._discos = discos
 
+    def colocar_disco(self, disco):
+        if self.get_tamanho() == 0:
+            self._discos.append(disco)
+        else:
+            if int(self.ultimo_disco()) > disco:
+                self._discos.append(disco)
+            else:
+                print('Não é possivel inserir esse disco, o disco abaixo é menor')
+
     def tirar_disco(self):
         return self._discos.pop()
 
     def get_tamanho(self):
         return len(self._discos)
+
+    def ultimo_disco(self):
+        return self._discos[len(self._discos) - 1]
 
     def primeiro_disco(self):
         return self._discos[0]
@@ -17,3 +29,6 @@ class Torre:
 
     def to_string(self):
         print('Torre: ' + self._torre)
+
+        for disco in list(reversed(self._discos)):
+            print(disco)
--- Hanoi/main.py
+++ Hanoi/main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+from Hanoi import Torre
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def string_torres():
+    print('')
+    # Torre A
+    torres[0].to_string()
+    # Torre B
+    torres[1].to_string()
+    # Torre C
+    torres[2].to_string()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('\nBem vindo a Torre de Hanoi!')
+    print('Regras: ')
+    print('1 - Movimentar uma só peça (disco) de cada vez.')
+    print('2 - Uma peça maior não pode ficar acima de uma menor.')
+    print('3 - Não é permitido movimentar uma peça que esteja abaixo de outra.\n')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    discos_jogo = int(input('Digite quantos discos(até 10): '))
+    discos = []
+    i = discos_jogo
+    while i > 0:
+        discos.append(i)
+        i -= 1
+    else:
+        print('Erro em inserir os discos, tente de novo!')
+
+    torres = [Torre('A', discos), Torre('B', []), Torre('C', [])]
